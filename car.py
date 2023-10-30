7964ff4b7d651d0cb6c2a12b5a564243985197ca
Long-Beard90 <126101510+Long-Beard90@users.noreply.github.com>
2023-10-30 23:23:09 +0330

    Update car.py

--- car.py
+++ car.py
@@ -1,11 +1,19 @@
 # this is a car that can start and stop . 
 command = ""
+car_started = False
 while True:
     command = input('>').lower()
     if command == "start":
-        print('the car started ...')
+        if car_started:
+            print('the car already started')
+        else:
+            car_started = True
+            print('the car started ...')
     elif command == "stop":
-        print('the car stopped ...')
+        if not car_started:
+            print('car is already stopped')
+        else:
+            print('the car stopped ...')
     elif command == "help":
         print("""
 start - to start the car
